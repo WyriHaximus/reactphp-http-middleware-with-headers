07b2b9a6eafcb76c5c78ddf7d4622d0539568f05
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-09-17 13:51:37 +0200

    WithHeadersMiddleware initial commit

---
+++ src/WithHeadersMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use function React\Promise\resolve;
+
+final class WithHeadersMiddleware
+{
+    private $headers = [];
+
+    /**
+     * @param array $headers
+     */
+    public function __construct(array $headers)
+    {
+        $this->headers = $headers;
+    }
+
+    public function __invoke(ServerRequestInterface $request, callable $next)
+    {
+        return resolve($next($request))->then(function (ResponseInterface $response) {
+            foreach ($this->headers as $header => $value) {
+                $response = $response->withHeader($header, $value);
+            }
+
+            return resolve($response);
+        });
+    }
+}
