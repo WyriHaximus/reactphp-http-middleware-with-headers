227ffeaab32b1bf6d5e6bcf530cbb8d30c0173ba
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-09-27 14:03:56 +0200

    Ditched the server namespace in both the package name and PHP namespaces

--- src/WithHeadersMiddleware.php
+++ src/WithHeadersMiddleware.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace WyriHaximus\React\Http\Server\Middleware;
+namespace WyriHaximus\React\Http\Middleware;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
